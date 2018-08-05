61a663c62bbb7e6cf5309e72631ffd9b7b73b008
Jason Huey <jason.huey1@gmail.com>
2018-08-05 16:28:21 -0700

    update

--- js/Main.js
+++ js/Main.js
@@ -1,1 +1,13 @@
-console.log("connected");
+$("#priority").on("change", addTask);
+
+function addTask() {
+
+	var taskName = $("#task")[0].value;
+	var rating = $("#priority")[0].options[priority.options.selectedIndex].value;
+
+	if(taskName) {
+		$("#taskList").append("<div class='col-6 py-5'><h3 class='display-4'>" + taskName + "</h3></div>");
+		$("#taskList").append("<div class='col-6 py-5'><h3 class='display-4'>" + rating + " stars</h3></div>");
+	}
+
+};
